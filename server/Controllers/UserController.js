169dc90ffbff2888e79d0196e983f02cf42f507f
aungcholatt <aungcholatt888@gmail.com>
2023-02-10 19:35:39 +0630

    loginUser Controller  completed

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,11 +1,11 @@
 import asyncHandler from 'express-async-handler';
 import User from '../Models/UserModel.js';
 import bcrypt from 'bcryptjs';
-import { UserRecord } from 'firebase-admin/lib/auth/user-record.js';
+import { generateToken } from '../middlewares/Auth.js'
 
-//@desc Register user
-//@route POST /api/users/
-//@access Public
+//@desc : Register user
+//@route : POST /api/users/
+//@access : Public
 const registerUser = asyncHandler(async (req, res) => {
     const {fullName, email, password, image} = req.body
     try {
@@ -37,10 +37,41 @@ const registerUser = asyncHandler(async (req, res) => {
             isAdmin: user.isAdmin,
             token: generateToken(user._id),
         });
+      } else {
+        res.status(400);
+        throw new Error("Invalid user data");
       }
     }catch(error){
-           
+      res.status(400).json({ message: error.message });
     }
 });
 
-export { registerUser };
+//@desc : Login user
+//@route : POST/api/users/
+//@access : Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  try{
+    //find user in DB
+    const user = await User.findOne({ email });
+    //if user exists compare password with hashed password then send user data and token to client
+    if (user && (await bcrypt.compare(password, user.password))){
+       res.json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        image: user.image,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+       });
+       //if user not found or password not match send error message
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+  } catch(error){
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export { registerUser, loginUser };
---
+++ server/middlewares/Auth.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+//@desc Authenticated user & get token
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    });
+};
+
+export { generateToken };
