bfce6ce53d0f4c42ebbe81bc342ed4143665211f
aungcholatt <aungcholatt888@gmail.com>
2023-02-11 01:03:34 +0630

    loginUser controller complete

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -47,7 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 //@desc : Login user
-//@route : POST/api/users/
+//@route : POST /api/users/
 //@access : Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
